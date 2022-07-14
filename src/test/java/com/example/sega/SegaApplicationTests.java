9398259321d42852df1ef3b38a1b7e53d5d90f85
khasim1643 <khasimjava43@gmail.com>
2022-07-14 17:08:39 +0530

    1st commit

---
+++ src/test/java/com/example/sega/SegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
